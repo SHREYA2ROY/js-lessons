d3f826c031e716516165d0984008b2e650ff83bc
SHREYA ROY <104238995+SHREYA2ROY@users.noreply.github.com>
2025-06-02 10:50:12 +0000

    datatypes config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+"use strict";// use all javascript code as newer version
+// alert(3+3);// we are using nodejs not browser , javascript engine is bound with browser window as well
+let age= 18;
+let name="oscar";
+let isLoggedIn = true;
+
+// types of datatypes 
+/* number =>*/
